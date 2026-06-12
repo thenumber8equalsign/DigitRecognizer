9a43d4e49ef06bd52af54289d592a166ae222c98
thenumber8equalsign <168995087+thenumber8equalsign@users.noreply.github.com>
2026-06-12 07:11:44 -0400

    Add a third hidden layer (I couldn't break 95% accuracy)

--- main.cpp
+++ main.cpp
@@ -148,10 +148,11 @@ void writeParamsToFile(const std::string& path, const std::vector<MachineLearnin
 
 void initializeLayers(std::mt19937& rng, MachineLearning::Model& model) {
     std::uniform_int_distribution<std::mt19937::result_type> dist(0,2000);
-    std::vector<std::shared_ptr<MachineLearning::Layer>> layers(1 + 2 + 1); // 2 hidden layers
+    std::vector<std::shared_ptr<MachineLearning::Layer>> layers(1 + 3 + 1);
     layers.at(0) = std::make_shared<MachineLearning::Layer>(28*28);
     layers.at(1) = std::make_shared<MachineLearning::Layer>(16);
     layers.at(2) = std::make_shared<MachineLearning::Layer>(16);
+    layers.at(3) = std::make_shared<MachineLearning::Layer>(16);
     layers.at(layers.size()-1) = std::make_shared<MachineLearning::Layer>(10);
 
     model.layers = layers;
